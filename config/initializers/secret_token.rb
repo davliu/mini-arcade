dfad4affc46c88fb31e36c4e13e963805215ea34
David Liu <davidliu@David-Lius-MacBook-Pro.local>
2012-09-08 15:37:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniArcade::Application.config.secret_token = '82467cca0cda7ca2c820444ecb992e376a0c8a890d531767883a7e4854a049d714fc8f10989f74b8571d46bda976edfb575916a1a0a532cc6d0f57bdcf13e59b'
